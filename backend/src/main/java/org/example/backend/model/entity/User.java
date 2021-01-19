734d8748e70e87519832777f7693fc6d78ee777b
Danil <dlyaregistr123@bk.ru>
2021-01-19 15:51:24 +0700

    фÐadded AbstractEntity class

--- backend/src/main/java/org/example/backend/model/entity/User.java
+++ backend/src/main/java/org/example/backend/model/entity/User.java
@@ -4,19 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "usr")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+public class User extends AbstractEntity {
     private String providerId;
     private String name;
     private String email;
     private String picture;
+
+    public User(Long id, String providerId, String name, String email, String picture) {
+        super(id);
+        this.providerId = providerId;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
 }
