293b1f43a275ddf8290309cecd887302c8162437
levin_main <dlyaregistr123@bk.ru>
2023-02-08 02:21:33 +0700

    detete problem test

--- backend/src/test/java/org/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/example/backend/BackendApplicationTests.java
@@ -1,21 +1,10 @@
 package org.example.backend;
 
-import org.example.backend.beanpostprocessor.SomeTestClass;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class BackendApplicationTests {
-    @Autowired
-    SomeTestClass someTestClass;
-    @Test
-    void contextLoads() {
-        System.out.println(someTestClass.getSomeValue());
-//        assertThat(someTestClass.getSomeValue()).isEqualTo(110);
-    }
-
 }
